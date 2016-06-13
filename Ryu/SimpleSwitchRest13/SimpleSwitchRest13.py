006f4f9e733469b5f22a38cfb1e7d21dac903ccb
YanHaoChen <coolking29@gmail.com>
2016-06-13 23:26:43 +0800

    SimpleSwitchRest13:build the code completely

--- Ryu/SimpleSwitchRest13/SimpleSwitchRest13.py
+++ Ryu/SimpleSwitchRest13/SimpleSwitchRest13.py
@@ -48,3 +48,39 @@ class SimpleSwitchRest13(simple_switch_13.SimpleSwitch13):
 				mac_table.update({entry_mac : entry_port})
 
 		return mac_table
+
+class SimpleSwitchController(ControllerBase):
+
+	def __init__(self, req, link, data, **config):
+		super(SimpleSwitchController, self).__init__(req, link, data, **config)
+		self.simple_switch_app = data[simple_switch_instance_name]
+
+	@route('simpleswitch', url, methods=['GET'], requirements={'dpid':dpid_lib.DPID_PATTERN})
+	def list_mac_table(self, req, **kwargs):
+		simple_switch = self.simple_switch_app
+		dpid = dpid_lib.str_to_dpid(kwargs['dpid'])
+
+		if dpid not in simple_switch.mac_to_port:
+			return Response(status=404)
+
+		mac_table = simple_switch.mac_to_port.get(dpid, {})
+		body = json.dumps(mac_table)
+
+		return Response(content_type='application/json', body=body)
+
+	@route('simpleswitch', url, methods=['PUT'], requirements={'dpid': dpid_lib.DPID_PATTERN})
+	def put_mac_table(self, req, **kwargs):
+
+		simple_switch = self.simple_switch_app
+		dpid = dpid_lib.str_to_dpid(lwargs['dpid'])
+		new_entry = eval(req.body)
+
+		if dpid not in simple_switch.mac_to_port:
+			returm Response(status=404)
+
+		try:
+			mac_table = simple_switch.set_mac_to_port(dpid, new_entry)
+			body = json.dumps(mac_table)
+			return Response(content_type='application/json', body=body)
+		except Exception as e:
+			return Response(status=500)
