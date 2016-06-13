3ae6ab6649280bc265f7a3463b39623f6cbe5461
YanHaoChen <coolking29@gmail.com>
2016-06-13 18:07:00 +0800

    TrafficMonitor:Update source code

--- Ryu/TrafficMonitor/SimpleMonitor.py
+++ Ryu/TrafficMonitor/SimpleMonitor.py
@@ -6,6 +6,8 @@ from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
 
+import json
+
 class SimpleMonitor(simple_switch_13.SimpleSwitch13):
 	def __init__(self, *args, **kwargs):
 		super(SimpleMonitor, self).__init__(*args, **kwargs)
@@ -43,33 +45,8 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
 	@set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
 	def _flow_stats_reply_handler(self, ev):
 		body = ev.msg.body
-		self.logger.info('datapath         '
-						 'in-port  eth-dst           '
-						 'out-port packets  bytes')
-		self.logger.info('---------------- '
-						 '-------- ----------------- '
-						 '-------- -------- --------')
-		for stat in sorted([flow for flow in body if flow.priority == 1],
-							key=lambda flow: (flow.match['in_port'],
-											  flow.match['eth_dst'])):
-			self.logger.info('%016x %8x %17s %8x %8d %8d',
-							 ev.msg.datapath.id,
-							 stat.match['in_port'], stat.match['eth_dst'],
-							 stat.instructions[0].actions[0].port,
-							 stat.packet_count, stat.byte_count)
+		self.logger.info('%s',json.dumps(ev.msg.to_jsondict(), ensure_ascii=True, indent=3, sort_keys=True))	
 
-	@set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
 	def _port_stats_reply_handler(self,ev):
 		body = ev.msg.body
-
-		self.logger.info('datapath         port     '
-						 'rx-pkts  rx-bytes rx-error '
-						 'tx-pkts  tx-bytes tx-error')
-		self.logger.info('---------------- -------- '
-						 '-------- -------- ------- '
-						 '-------- -------- -------')
-		for stat in sorted(body, key=attrgetter('port_no')):
-			self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
-							 ev.msg.datapath.id, stat.port_no,
-							 stat.rx_packets, stat.rx_bytes, stat.rx_errors,
-							 stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+		self.logger.info('%s',json.dumps(ev.msg.to_jsondict(), ensure_ascii=True, indent=3, sort_keys=True))	
